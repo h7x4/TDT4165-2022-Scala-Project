94bb828dad7770e9ac14c4212148a4e03ed87e11
h7x4 <h7x4@nani.wtf>
2022-10-17 18:37:18 +0200

    Make task 1 easier to run

--- tasks/task1.scala
+++ tasks/task1.scala
@@ -1,29 +1,38 @@
+object Task1A extends App {
+  val x = (1 to 50).toList
 
-object Main extends App {
-    //task a
-    val x = (1 to 50).toList
-    println(x)
+  println(x)
+}
 
-    //task b
-    def find_sum_of_list(list: List[Int]): Int = {
-        var sum: Int = 0
-        list.foreach (sum += _)
-        sum
-    }
-    println(find_sum_of_list(List(1, 2, 3, 4)))
+object Task1B extends App {
+  def find_sum_of_list(list: List[Int]): Int = {
+    var sum: Int = 0
+    list.foreach (sum += _)
+    sum
+  }
 
-    //task c
-    def find_sum_of_list_rec(list: List[Int]): Int = list match{
-        case List() => 0
-        case _ => list.head + find_sum_of_list_rec(list.tail)
-    }
-    println(find_sum_of_list_rec(List(1, 2, 3, 4)))
+  println(find_sum_of_list(List(1, 2, 3, 4)))
+}
 
-    //task d
-    def nth_fibonacci(n: BigInt): BigInt = n match 
-        case 0 => 0
-        case 1 => 1
-        case _ => nth_fibonacci(n-1) + nth_fibonacci(n-2)
+object Task1C extends App {
+  //task c
+  def find_sum_of_list_rec(list: List[Int]): Int = list match{
+    case List() => 0
+    case _ => list.head + find_sum_of_list_rec(list.tail)
+  }
+
+  println(find_sum_of_list_rec(List(1, 2, 3, 4)))
+}
     
-    println(nth_fibonacci(10))   
+object Task1D extends App {
+  val bigZero = BigInt(0)
+  val bigOne = BigInt(1) // 🍕
+
+  def nth_fibonacci(n: BigInt): BigInt = n match {
+    case `bigZero` => n
+    case `bigOne` => n
+    case _ => nth_fibonacci(n-1) + nth_fibonacci(n-2)
+  }
+  
+  println(nth_fibonacci(30))   
 }
